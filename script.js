cb82fd070b514bd21b434ecf2b213879fd2de1e1
Mdidu <56021893+Mdidu@users.noreply.github.com>
2019-10-08 16:23:40 +0200

    cv first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var cv = document.getElementById('CV');
+var leftBloc = document.getElementById('leftBloc');
+var color = true;
+cv.addEventListener('click', function (){
+    if(color){
+        this.style.backgroundColor = 'black';
+        this.style.color = 'white';
+        this.style.boxShadow = '10px 5px 5px gray';
+        leftBloc.style.borderRight = '2px solid white';
+        color = false;
+    }else {
+        this.style.backgroundColor = 'white';
+        this.style.color = 'black';
+        this.style.boxShadow = '10px 5px 5px black';
+        leftBloc.style.borderRight = '2px solid black';
+        color = true;
+    }
+});
