b0a3c09e986d0f9c9e8543cdca6b17fad2575a1d
Mdidu <56021893+Mdidu@users.noreply.github.com>
2019-10-09 11:34:49 +0200

    cv switch color page

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+/*
+Améliorations possible: lors du clic sur un lien ne pas changer les couleurs sur la page.
+Intervertir les blocs dans le cv et conserver leurs positions
+
+ */
 var cv = document.getElementById('CV');
+var body = document.getElementById('body');
 var leftBloc = document.getElementById('leftBloc');
+// var bar = document.getElementsByClassName('bar');
 var color = true;
 
-cv.addEventListener('click', function (){
+// function adding an event click on the page to change the colors on the page
+body.addEventListener('click', function (){
 
     if(color){
-        this.style.backgroundColor = 'black';
-        this.style.color = 'white';
-        this.style.boxShadow = '10px 5px 5px gray';
+        cv.style.backgroundColor = 'black';
+        this.style.backgroundColor = 'white';
+        // bar.style.border = "1px solid white";
+        cv.style.color = 'white';
+        //cv.style.boxShadow = '10px 5px 5px gray';
         leftBloc.style.borderRight = '2px solid white';
         color = false;
+        // bar.style.border = '1px solid white';
     }else {
-        this.style.backgroundColor = 'white';
-        this.style.color = 'black';
-        this.style.boxShadow = '10px 5px 5px black';
+        cv.style.backgroundColor = 'white';
+        this.style.backgroundColor = 'black';
+        //bar.style.border = "1px solid white";
+        cv.style.color = 'black';
+        //cv.style.boxShadow = '10px 5px 5px gray';
         leftBloc.style.borderRight = '2px solid black';
         color = true;
     }
